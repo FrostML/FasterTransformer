a0a14bdbfd2add0361f2176bbab1d19fdae3d048
FrostML <380185688@qq.com>
2021-06-03 05:59:18 +0000

    123

--- fastertransformer/cuda/decoding_kernels.cu
+++ fastertransformer/cuda/decoding_kernels.cu
@@ -208,7 +208,7 @@ namespace fastertransformer
         int src_ite = ite - mem_len + memory_sequence_length[bid];
         // int src_id = bid * mem_len * n_head * size_per_head + src_ite * n_head * size_per_head +
         //             head_id * size_per_head + tid;
-        int src_id = bid * n_head * mem_len * size_per_head + head_id * n_head * size_per_head +
+        int src_id = bid * n_head * mem_len * size_per_head + head_id * mem_len * size_per_head +
                     src_ite * size_per_head + tid;
         k_tgt[ite * offset + tgt_id] = (cache_k[src_id]);
         v_tgt[ite * offset + tgt_id] = (cache_v[src_id]);
